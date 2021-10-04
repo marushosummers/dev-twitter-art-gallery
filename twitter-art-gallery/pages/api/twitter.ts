353e81ed2154dae69b0b60023dac51d3762d2cc8
marusho <mrsho34@gmail.com>
2021-10-04 15:48:54 +0000

    add twitter modules

---
+++ twitter-art-gallery/pages/api/twitter.ts
@@ -0,0 +1,23 @@
+import TwitterApi from "twitter-api-v2";
+
+const token = process.env.APP_USER_TOKEN ?? "";
+
+const client = new TwitterApi(token);
+
+const image = async () => {
+  const params = { screen_name: "marushosummers", count: 1 };
+  await client
+    .get("favorites/list", params)
+    .then((tweet) => {
+      console.log(tweet);
+    })
+    .catch((error) => {
+      throw error;
+    });
+  return {
+    statusCode: 200,
+    body: "success!"
+  };
+};
+
+export default image;
