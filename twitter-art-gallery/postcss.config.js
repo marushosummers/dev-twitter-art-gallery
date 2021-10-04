5f1d83c5d4b062900b6944eb76f352fa2db3dc4a
marusho <mrsho34@gmail.com>
2021-10-05 00:02:58 +0900

    RCAで作成準備

---
+++ twitter-art-gallery/postcss.config.js
@@ -0,0 +1,20 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./public/**/*.html', './src/**/*.tsx', './src/**/*.jsx'],
+  defaultExtractor: (content) => {
+    // Capture as liberally as possible, including things like `h-(screen-1.5)`
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+
+    return broadMatches.concat(innerMatches);
+  },
+});
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
+};
--- twitter-art-gallery/src/App.tsx
+++ twitter-art-gallery/src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container mx-auto">
+      <header className="flex justify-center items-center text-3xl h-32 mx-5">
+        hello :D
       </header>
+      <div className="flex justify-center">
+        content
+      </div>
     </div>
   );
 }
