cef956c5fccaca378f5981461079be3e681757e1
marusho <mrsho34@gmail.com>
2021-10-05 00:55:22 +0900

    fix

--- twitter-art-gallery/pages/api/twitter.ts
+++ twitter-art-gallery/pages/api/twitter.ts
@@ -5,7 +5,7 @@ const token = process.env.APP_USER_TOKEN ?? "";
 const client = new TwitterApi(token);
 
 const image = async () => {
-  const params = { screen_name: "marushosummers", count: 1 };
+  const params = { screen_name: "marusho_summers", count: 1 };
   await client
     .get("favorites/list", params)
     .then((tweet) => {
