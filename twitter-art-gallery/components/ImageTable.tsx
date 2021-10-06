cd74d04b1f7c7262410f961fa9489cc32329a9da
marusho <mrsho34@gmail.com>
2021-10-06 16:55:27 +0900

    無限スクロールの修正

--- twitter-art-gallery/components/ImageTable.tsx
+++ twitter-art-gallery/components/ImageTable.tsx
@@ -29,6 +29,17 @@ class ImageTable extends React.Component<typeImageTableProps, typeImageTableStat
         };
     }
 
+    componentDidMount() {
+        let queue: NodeJS.Timeout;
+        window.addEventListener("resize", () => {
+            clearTimeout(queue);
+            queue = setTimeout(() => {
+                const raneNum = window.innerWidth > 600 ? Math.floor(window.innerWidth / 300) : 2;
+                this.setState({ raneNum: raneNum });
+            }, 500);
+        });
+    }
+
     render() {
         return (
             <div className="flex m-1">
