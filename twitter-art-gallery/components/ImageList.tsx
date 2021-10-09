90b6aaf1e92aabdb0e91570b22d5e400e7a61d07
marusho <mrsho34@gmail.com>
2021-10-09 05:10:38 +0000

    imageタグに変更

--- twitter-art-gallery/components/ImageList.tsx
+++ twitter-art-gallery/components/ImageList.tsx
@@ -1,40 +1,41 @@
 import React from "react";
+import Image from 'next/image'
 
 type typeRaneItems = {
-    url: string;
-    source: string;
+  url: string;
+  source: string;
 };
 type typeImageListProps = {
-    raneItems: typeRaneItems[];
+  raneItems: typeRaneItems[];
 };
 
 class ImageList extends React.Component<typeImageListProps> {
-    listItems = (items: typeRaneItems[]) => {
-        return (
-            <div className="flex flex-col">
-                {items.map((item: typeRaneItems, index: number) => {
-                    return (
-                        <div key={index}>
-                            <div className="m-1 max-w-xs">
-                                <ListItem url={item.url} source={item.source} />
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
-        );
-    };
+  listItems = (items: typeRaneItems[]) => {
+      return (
+          <div className="flex flex-col">
+              {items.map((item: typeRaneItems, index: number) => {
+                  return (
+                      <div key={index}>
+                          <div className="m-1 max-w-xs">
+                              <ListItem url={item.url} source={item.source} />
+                          </div>
+                      </div>
+                  );
+              })}
+          </div>
+      );
+  };
 
-    render() {
-        return <div>{this.listItems(this.props.raneItems)}</div>;
-    }
+  render() {
+      return <div>{this.listItems(this.props.raneItems)}</div>;
+  }
 }
 export default ImageList;
 
-function ListItem(props: typeRaneItems) {
-    return (
-        <a href={props.source} target="_blank" rel="noopener noreferrer">
-            <img src={props.url} alt=""/>
-        </a>
-    );
+const ListItem = (props: typeRaneItems) => {
+  return (
+    <a href={props.source} target="_blank" rel="noopener noreferrer">
+      <Image src={props.url} height={600} width={300}/>
+    </a>
+  );
 }
--- twitter-art-gallery/next.config.js
+++ twitter-art-gallery/next.config.js
@@ -4,4 +4,7 @@ module.exports = {
   env: {
     ENDPOINT: process.env.ENDPOINT,
   },
+  images: {
+    domains: ['pbs.twimg.com'],
+  },
 }
