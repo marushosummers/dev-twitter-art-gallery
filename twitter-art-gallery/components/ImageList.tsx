812e39be3982dd3eee0ded0959376de3bf1ab620
marusho <mrsho34@gmail.com>
2021-10-09 05:28:17 +0000

    不要なdivを削除

--- twitter-art-gallery/components/ImageList.tsx
+++ twitter-art-gallery/components/ImageList.tsx
@@ -12,20 +12,18 @@ interface ImageListProps {
 
 const ImageList: React.FC<ImageListProps> = ({ imageItems }) => {
   return (
-    <div>
-      <div className="flex flex-col">
-        {imageItems.map((image: ImageItem, index: number) => {
-          return (
-            <div key={index}>
-              <div className="m-1 max-w-xs">
-                <a href={image.source} target="_blank" rel="noopener noreferrer">
-                  <Image src={image.url} height={600} width={300} />
-                </a>
-              </div>
+    <div className="flex flex-col">
+      {imageItems.map((image: ImageItem, index: number) => {
+        return (
+          <div key={index}>
+            <div className="m-1 max-w-xs">
+              <a href={image.source} target="_blank" rel="noopener noreferrer">
+                <Image src={image.url} height={600} width={300} />
+              </a>
             </div>
-          );
-        })}
-      </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
