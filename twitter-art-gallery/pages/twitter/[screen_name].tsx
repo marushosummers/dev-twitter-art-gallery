392c01e057d8d8612bf2ab8d155851792fbe59c0
marusho <mrsho34@gmail.com>
2021-10-11 17:36:37 +0000

    いいねを実装

--- twitter-art-gallery/pages/twitter/[screen_name].tsx
+++ twitter-art-gallery/pages/twitter/[screen_name].tsx
@@ -1,9 +1,18 @@
 import { useRouter } from "next/router";
+import Image from 'next/image'
 import MainTable from "../../components/MainTable";
 import Layout from '../../components/layout'
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Header from "../../components/Header";
+
+
+interface typeUser {
+  id: number
+  name: string
+  image: string
+  favourites_count: number
+};
 
 interface typeImages {
   images: ImageItem[];
@@ -23,6 +32,7 @@ export type ImageItem = {
 const TwitterScreenName = () => {
   const router = useRouter();
   const { screen_name } = router.query;
+
   let name: string;
   if ((typeof screen_name === "string")) {
     name = screen_name;
@@ -32,6 +42,7 @@ const TwitterScreenName = () => {
 
   const [screenName, setScreenName] = useState(name)
   const [images, setImages] = useState([])
+  const [icon, setIcon] = useState("")
   const [message, setMessage] = useState("loading...")
   const [loading, setLoading] = useState(true)
   const [maxId, setMaxId] = useState(0)
@@ -39,13 +50,22 @@ const TwitterScreenName = () => {
   useEffect(() => {
     if (!router.isReady) return;
     setScreenName(name)
+    getUser(name)
     getiine(name)
   }, [router.isReady, name])
 
+  const getUser = async (name: string): Promise<void> => {
+    try {
+      const response: any = await userAPI(name)
+      setIcon(response.body.user.image)
+    } catch {
+      setMessage("取得に失敗しました。データが空か、スクリーンネームが間違っているかもしれません。");
+    }
+  }
+
   const getiine = async (name: string): Promise<void> => {
     try {
-      const response: any = await twitterAPI(name, maxId)
-      console.log(response.body)
+      const response: any = await favAPI(name, maxId)
       setIineImages(response.body)
     } catch {
       setMessage("取得に失敗しました。データが空か、スクリーンネームが間違っているかもしれません。");
@@ -63,7 +83,21 @@ const TwitterScreenName = () => {
     setLoading(false);
   };
 
-  const twitterAPI = async (screen_name: string, max_id: number): Promise<typeImages> => {
+  const userAPI = async (screen_name: string): Promise<typeUser> => {
+    let endpoint = `${process.env.ENDPOINT}/api/twitter/user?name=${screen_name}`
+
+    console.log(endpoint)
+
+    try {
+      const response: any = await axios.get(endpoint);
+      return response.data
+    } catch (error) {
+      console.log(error)
+      throw (Error)
+    }
+  }
+
+  const favAPI = async (screen_name: string, max_id: number): Promise<typeImages> => {
     let endpoint = `${process.env.ENDPOINT}/api/twitter/fav?name=${screen_name}`
     if (max_id) {
       endpoint += `&max_id=${max_id}`
@@ -98,9 +132,21 @@ const TwitterScreenName = () => {
       <Header name={screenName} />
       <div className="min-h-screen" >
         <div className="container mx-auto" >
-          <div className="flex justify-center items-center text-3xl h-32 mx-5" >
-            <div className="nm-flat-gray-100 p-3 rounded-xl" >
-              @{screenName}
+          <div className="flex justify-center items-center" >
+            <div className="nm-flat-gray-100 flex justify-center items-center p-2 mb-8 rounded-full" >
+              <Image
+                src={icon}
+                width={48}
+                height={48}
+                className="rounded-full"
+              />
+            </div>
+          </div>
+          <div className="flex justify-center items-center" >
+            <div className="nm-inset-gray-100 flex justify-center items-center p-2 mb-8 rounded-full" >
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="#F91780">
+                <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+              </svg>
             </div>
           </div>
           <div className="flex justify-center" >
