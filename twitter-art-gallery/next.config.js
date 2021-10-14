07b39ddd38e587304f09575c05a69bf53de04d76
marusho <mrsho34@gmail.com>
2021-10-14 11:01:42 +0000

    キャッシュ時間を変更

--- twitter-art-gallery/next.config.js
+++ twitter-art-gallery/next.config.js
@@ -4,4 +4,22 @@ module.exports = {
   env: {
     ENDPOINT: process.env.ENDPOINT,
   },
+  images: {
+        minimumCacheTTL: 60,
+    domains: ['pbs.twimg.com','abs.twimg.com'],
+  },
+ async headers () {
+   return [
+     {
+      source: '/(.*).(jpg|png)',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value:
+            'public, max-age=30, s-maxage=30',
+        },
+      ],
+     }
+   ]
+  }
 }
